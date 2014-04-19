46d719abd9063837fb0fc7f480fb2f2e9ccf366b
cheptsov <andrey.cheptsov@jetbrains.com>
2014-04-19 14:14:13 +0400

    Deployment bugfix

--- src/main/kotlin/kotlinx/nosql/mongodb/samples/petclinic/controllers/AppConfig.kt
+++ src/main/kotlin/kotlinx/nosql/mongodb/samples/petclinic/controllers/AppConfig.kt
@@ -10,11 +10,6 @@ import kotlinx.nosql.CreateDrop
 Configuration open class AppConfig {
     Bean open fun getMongoDB(): MongoDB {
         val mongoURI = MongoClientURI(System.getenv("MONGOHQ_URL"))
-        val database: String = if (mongoURI.getDatabase() != null) mongoURI.getDatabase()!! else "test"
-        val username: String = if (mongoURI.getUsername() != null) mongoURI.getUsername()!! else ""
-        val password: String = if (mongoURI.getPassword() != null) mongoURI.getPassword().toString() else ""
-        println("MONGOHQ_URL = $mongoURI")
-        println("database: ${database}; username: $username; password: $password")
         val mongoDb = MongoDB(mongoURI, schemas = array(Owners, Pets, PetTypes, Vets, Visits), initialization = CreateDrop())
         return mongoDb
     }
